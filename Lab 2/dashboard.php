0e799b910bfd5dcf34f05977a5dc8b6aa1bbe80a
Bokhtiar Rifat <63875590+rifatiyaan@users.noreply.github.com>
2021-03-28 22:10:07 +0600

    completed

---
+++ Lab 2/dashboard.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	<title> Dashboard </title>
+	</head>
+	
+	<body>
+	<center>
+	<div> <a href =""> All Student </a></div>
+	<div><a href =""> Add student </a></div>
+	<div><a href =""> All department </a> </div>
+	<div><a href =""> Add Department </a></div>
+	</center>
+	</body>
+	
+</html>
